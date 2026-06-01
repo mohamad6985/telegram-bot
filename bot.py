0b4d041f92f030d17f2efdab9b7eb4bf695940bb
mohamad6985 <mohamadmahboob36@gmail.com>
2026-06-02 00:09:54 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,25 @@
 import os
-import yt_dlp
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
+import yt_dlp
 
 TOKEN = os.getenv("8863865830:AAGHRVy-MgesNn-thoEx4_3XicqBBI6Vh6k")
 
 
-def download_audio(query):
+def get_audio_url(query):
     ydl_opts = {
         "format": "bestaudio/best",
         "noplaylist": True,
         "quiet": True,
         "default_search": "ytsearch1",
-        "outtmpl": "song.%(ext)s",
-        "postprocessors": [{
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "192",
-        }],
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([query])
+        info = ydl.extract_info(query, download=False)
+        if "entries" in info:
+            info = info["entries"][0]
 
-    return "song.mp3"
+        return info["url"], info.get("title", "audio")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -33,15 +29,15 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.message.text
 
-    await update.message.reply_text("⏳ در حال دانلود...")
+    await update.message.reply_text("⏳ در حال پیدا کردن...")
 
     try:
-        file_path = download_audio(query)
+        url, title = get_audio_url(query)
 
-        with open(file_path, "rb") as audio:
-            await update.message.reply_audio(audio=audio)
-
-        os.remove(file_path)
+        await update.message.reply_audio(
+            audio=url,
+            title=title
+        )
 
     except:
         await update.message.reply_text("❌ خطا دوباره تلاش کن")
