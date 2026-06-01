43e9c257fa90df6daf45e39bf6940230d69530d3
mohamad6985 <mohamadmahboob36@gmail.com>
2026-06-01 22:21:35 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,8 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
-from urllib.parse import quote
+import yt_dlp
+import os
 
 TOKEN = "8863865830:AAGHRVy-MgesNn-thoEx4_3XicqBBI6Vh6k"
 
-async def search_music(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-
-    query = quote(text)
-    url = f"https://www.youtube.com/results?search_query={query}"
-
-    await update.message.reply_text(
-        f"🎵 نتیجه سرچ:\n{url}"
-    )
-
-app = Application.builder().token(TOKEN).build()
-
-app.add_handler(MessageHandler(filters.TEXT, search_music))
-
-print("Bot Started...")
-app.run_polling(drop_pending_updates=True)
+CHANNEL = "https://t.me/YourChannel"
